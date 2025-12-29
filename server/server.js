963c0c7b93cdd952075a0fe78157f71b80844d3f
Parasuram76 <parasuramsaikumar@gmail.com>
2025-12-29 18:51:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ dotenv.config()
 const PORT = 5000 ;
 const app = express() ;
 app.use(cors({
-    origin : 'http://localhost:5173',
+    origin : 'https://task-management-system-1-d407.onrender.com',
     credentials : true
 }))
 app.use(express.json())
